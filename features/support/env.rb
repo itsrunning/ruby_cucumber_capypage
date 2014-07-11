a53a7e182e1cb8e342f01467de9e00c77f4ebead
chris_rex <christr@thoughtworks.com>
2014-07-11 18:17:54 +0530

    [Christopher] Added capture of screenshot if cucumber html report is generated

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,8 +1,10 @@
 require 'bundler'
 Bundler.require
+require 'capybara-screenshot/cucumber'
 
 require_relative 'page_helper'
 
 World(PageHelper)
 
 Capybara.default_driver = :selenium
+Capybara.save_and_open_page_path = File.dirname(__FILE__) + '/../screenshots/'
